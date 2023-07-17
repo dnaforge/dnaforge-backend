8e02b15e3c7d28fd3f0f1fbcf4975bb22bf11263
LeWimbes <12753781+LeWimbes@users.noreply.github.com>
2023-07-17 16:14:27 +0300

    Immediately start execution of remaining jobs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,14 @@
 import org.slf4j.LoggerFactory
+import sim.Jobs
 import web.startWebServer
 
 
 private val log = LoggerFactory.getLogger("Main")
 
 fun main(): Unit = try {
+    // force initialization of Jobs and thus the start of the job worker
+    Jobs
+
     startWebServer()
 } catch (e: Throwable) {
     // Make sure that all unexpected exceptions are logged (except for exceptions in the logger).
