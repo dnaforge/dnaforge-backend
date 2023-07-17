734ef50e166ac77e3e672deac9f871755e867280
LeWimbes <12753781+LeWimbes@users.noreply.github.com>
2023-07-17 15:40:26 +0300

    Change default logging level to INFO. Add environment variable to specify the logging level.

--- src/main/kotlin/Environment.kt
+++ src/main/kotlin/Environment.kt
@@ -1,3 +1,6 @@
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.LoggerContext
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.io.File
 
@@ -34,6 +37,14 @@ object Environment {
     val port: Int
 
     init {
+        // update log level
+        val logLevel: String? = System.getenv("LOGLEVEL")
+        if (logLevel != null) {
+            val loggerContext = LoggerFactory.getILoggerFactory() as LoggerContext
+            val rootLogger = loggerContext.getLogger(Logger.ROOT_LOGGER_NAME)
+            rootLogger.level = Level.toLevel(logLevel, rootLogger.level)
+        }
+
         log.debug("Initializing environment.")
 
         // Reading environment variables and arguments
