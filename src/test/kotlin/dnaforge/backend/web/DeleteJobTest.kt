443b15ebaacd64edec05b2d98e1362ea1c8b4b42
LeWimbes <12753781+LeWimbes@users.noreply.github.com>
2023-07-20 13:29:23 +0300

    Add tests for deleting jobs

---
+++ src/test/kotlin/dnaforge/backend/web/DeleteJobTest.kt
@@ -0,0 +1,56 @@
+package dnaforge.backend.web
+
+import dnaforge.backend.InternalAPI
+import dnaforge.backend.sim.Jobs
+import dnaforge.backend.sim.default
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.BeforeEach
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DeleteJobTest {
+
+    @OptIn(InternalAPI::class)
+    @BeforeEach
+    fun `prepare app state`() {
+        File("./data").deleteRecursively()
+        Jobs.resetState()
+        Jobs.inhibitJobExecution()
+    }
+
+    @Test
+    fun `deleting job doesn't work without auth`() = testApplication {
+        val client = prepare()
+
+        client.delete("/job/0").apply {
+            assertEquals(HttpStatusCode.Unauthorized, status)
+        }
+    }
+
+    @Test
+    fun `deleting job doesn't work with wrong id`() = testApplication {
+        val (client, bearerToken) = prepareWithAuth()
+
+        client.delete("/job/0") {
+            header(HttpHeaders.Authorization, bearerToken)
+        }.apply {
+            assertEquals(HttpStatusCode.NotFound, status)
+        }
+    }
+
+    @Test
+    fun `deleting job works`() = testApplication {
+        val (client, bearerToken) = prepareWithAuth()
+
+        Jobs.submitNewJob(default, "a", "b", "c")
+
+        client.delete("/job/0") {
+            header(HttpHeaders.Authorization, bearerToken)
+        }.apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
+    }
+}
