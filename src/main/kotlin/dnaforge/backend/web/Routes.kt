af30324c3758313ed4310e726162a5bcd4afe302
LeWimbes <12753781+LeWimbes@users.noreply.github.com>
2023-07-26 10:07:19 +0300

    Migrate from deprecated CORS plugin.

--- src/main/kotlin/dnaforge/backend/web/Routes.kt
+++ src/main/kotlin/dnaforge/backend/web/Routes.kt
@@ -9,9 +9,9 @@ import dnaforge.backend.zipFileName
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.partialcontent.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.request.*
